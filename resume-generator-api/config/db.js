ba1b75d22580b078bf2e8552a9c6e936ec71f87c
S.A.Yasir <yasirsa05@gmail.com>
2026-02-10 16:20:31 +0530

    now completed laptop neon db connection

--- resume-generator-api/config/db.js
+++ resume-generator-api/config/db.js
@@ -1,14 +1,41 @@
-const {Pool} = require("pg");
-require("dotenv").config()
+// const {Pool} = require("pg");
+// require("dotenv").config()
 
-const pool =new Pool({
+// const pool =new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+//   ssl: false   // local postgresக்கு
+
+// })
+
+// module.exports = pool;
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  ssl: false   // local postgresக்கு
+  ssl: {
+    rejectUnauthorized: false // required for Neon
+  }
+});
 
-})
+pool.connect()
+  .then(() => console.log("Connected to Neon DB ✅",{user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true // required for Neon
+  }
+}))
+  .catch(err => console.error("Connection error to Neon DB ❌", err));
 
 module.exports = pool;
