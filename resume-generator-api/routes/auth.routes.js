442be42bd4ebf7111ceaa2022f86e73d36e01144
S.A.Yasir <yasirsa05@gmail.com>
2026-05-02 08:19:44 +0530

    now local is ok

--- resume-generator-api/routes/auth.routes.js
+++ resume-generator-api/routes/auth.routes.js
@@ -131,16 +131,18 @@ router.get(
   })
 );
 
-// 🔹 Google OAuth callback
+
 router.get(
   "/auth/google/callback",
   passport.authenticate("google", {
-    failureRedirect: "/login",
+    failureRedirect:
+      process.env.NODE_ENV === "production"
+        ? `${process.env.FRONTEND_URL}/login`
+        : "http://localhost:5000/login",
     session: false,
   }),
   googlelogin
 );
-
 // 🔹 Normal Auth
 router.get(
   "/getChatMessages/:chattitle_id",
@@ -192,7 +194,7 @@ router.post(
   saveInterviewResults
 );
 router.post("/download-pdf", downloadPDF);
-router.post("/auth/interview-results", getInterviewResults);
+router.post("/auth/interview-results", verifyToken, getInterviewResults);
 router.post('/interview/store-context', storeContext);
 router.get("/getresumeformock", verifyToken, getresumesformock);
 router.post("/interview/chat",sendinterviewchat)
