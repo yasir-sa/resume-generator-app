2e807bb75ce6e393b5ed41325a0c99818fe74677
S.A.Yasir <yasirsa05@gmail.com>
2026-05-02 07:06:36 +0530

    ok

--- resume-generator-ui/vite.config.js
+++ resume-generator-ui/vite.config.js
@@ -1,7 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // build path crct-ah irukannu check pannikonga
+  build: {
+    outDir: 'dist',
+  }
 })
