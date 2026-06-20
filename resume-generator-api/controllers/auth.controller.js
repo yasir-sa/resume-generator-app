23be0c085d1d3fafbe688a498279321b5b2f04c9
asiyasa <asiyasa07@gmail.com>
2026-06-20 07:27:31 +0530

    now controller chnage for puppeeter use only chrome based but render not have chrome so this change

--- resume-generator-api/controllers/auth.controller.js
+++ resume-generator-api/controllers/auth.controller.js
@@ -3594,7 +3594,13 @@ exports.downloadPDF = async (req, res) => {
 
     const browser = await puppeteer.launch({
       headless: "new",
-      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-gpu",
+        "--single-process"
+      ]
     });
 
     const page = await browser.newPage();
